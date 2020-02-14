edf05f6a153713ca9d81c36b217607316e7b2ae0
Mr.Miss | 사나 ❤️ <gaung@bagaswara.com>
2020-02-14 15:45:37 +0700

    lessen the afk auto del time

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -141,7 +141,7 @@ async def type_afk_is_not_true(notafk):
     if ISAFK:
         ISAFK = False
         msg = await notafk.respond("I'm no longer AFK.")
-        await sleep(3)
+        await sleep(1)
         await msg.delete()
         if BOTLOG:
             await notafk.client.send_message(
