b8c93845d94597e70cd770bdd0c5ff517d333f84
Mr.Miss <gaung@bagaswara.com>
2020-03-09 15:20:09 +0700

    create: ".create b" won't kick the bot anymore
    Signed-off-by: Mr.Miss <gaung@bagaswara.com>

--- userbot/modules/create.py
+++ userbot/modules/create.py
@@ -23,14 +23,10 @@ async def telegraphs(grop):
                     title=group_name
                 ))
                 created_chat_id = result.chats[0].id
-                await grop.client(functions.messages.DeleteChatUserRequest(
-                    chat_id=created_chat_id,
-                    user_id="@sanaTWICEbot"
-                ))
                 result = await grop.client(functions.messages.ExportChatInviteRequest(
                     peer=created_chat_id,
                 ))
-                await grop.edit("Your `{}` Group Created Successfully. Click [{}]({}) to join".format(group_name, group_name, result.link))
+                await grop.edit("Your {} Group Created Successfully. Click [{}]({}) to join".format(group_name, group_name, result.link))
             except Exception as e:  # pylint:disable=C0103,W0703
                 await grop.edit(str(e))
         elif type_of_group == "g" or type_of_group == "c":
@@ -44,7 +40,7 @@ async def telegraphs(grop):
                 result = await grop.client(functions.messages.ExportChatInviteRequest(
                     peer=created_chat_id,
                 ))
-                await grop.edit("Your `{}` Group/Channel Created Successfully. Click [{}]({}) to join".format(group_name, group_name, result.link))
+                await grop.edit("Your {} Group/Channel Created Successfully. Click [{}]({}) to join".format(group_name, group_name, result.link))
             except Exception as e:  # pylint:disable=C0103,W0703
                 await grop.edit(str(e))
 
