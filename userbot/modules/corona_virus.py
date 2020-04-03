17f312e9faef170b3d54855db336d10a7cce0d30
Mr.Miss <gaung@bagaswara.com>
2020-04-03 16:18:31 +0700

    corona_virus: update and fix
    Signed-off-by: Mr.Miss <gaung@bagaswara.com>

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -5,31 +5,34 @@
 #
 # Port to UserBot by @MoveAngel
 
+from datetime import datetime
 from covid import Covid
 from userbot import CMD_HELP
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.covid(?: |$)(.*)")
+@register(outgoing=True, pattern="^.covid (.*)")
 async def corona(event):
+    await event.edit("`Processing...`")
+    country = event.pattern_match.group(1)
     covid = Covid()
-    data = covid.get_data()
-    input_str	 = event.pattern_match.group(1)
-    country = input_str.capitalize()
-    country_data = get_country_data(country, data)
-    output_text = "" 
-    for name, value in country_data.items():
-        output_text += "`{}`: `{}`\n".format(str(name), str(value))
-    await event.edit("**CoronaVirus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
-
-def get_country_data(country, world):
-    for country_data in world:
-        if country_data["country"] == country:
-            return country_data
-    return {"Status": "No information yet about this country!"}
+    country_data = covid.get_status_by_country_name(country)
+    if country_data:
+        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
+        output_text += f"`Active      : {country_data['active']}`\n"
+        output_text += f"`Deaths      : {country_data['deaths']}`\n"
+        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text += (
+            "`Last update : "
+            f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
+        )
+        output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
+    else:
+        output_text = "No information yet about this country!"
+    await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
 
 
 CMD_HELP.update({
         "covid": 
-        ".covid <country>\
-          \nUsage: Get an information about data covid-19 in your country."
+        ".covid <country>"
+        "\nUsage: Get an information about data covid-19 in your country.\n"
     })
