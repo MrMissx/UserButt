d089238a1fb05faa895ab846b3bd7001a551c137
Mr.Miss <gaung@bagaswara.com>
2020-04-13 10:02:44 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -93,7 +93,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "Support chat PM: @adekmaulana"
+                    link = "[Support group](https://t.me/userbotindo)"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
                     text += "Nothing is logged except the fact of error and date\n"
