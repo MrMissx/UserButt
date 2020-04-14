b5d833c017ddeafab3f990259b4e1116fedfa0d4
Mr.Miss <gaung@bagaswara.com>
2020-04-14 10:35:59 +0700

    heroku: fix derp
    Signed-off-by: Mr.Miss <gaung@bagaswara.com>

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -96,7 +96,6 @@ async def variable(var):
                     "`Successfully changed...`"
                 )
             await var.edit("`Information sets...`")
-            heroku_var[variable] = value
         else:
             if BOTLOG:
                 await var.client.send_message(
@@ -108,7 +107,8 @@ async def variable(var):
                     f"     • `{value}`\n\n"
                     "`Successfully added...`"
                 )
-            return await var.edit("`Information added...`")
+            await var.edit("`Information added...`")
+        heroku_var[variable] = value
     elif exe == "del":
         await var.edit("`Getting and setting information...`")
         try:
