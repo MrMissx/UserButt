916c20c803a74bce0260056a206174fe7dc60f0c
Mr.Miss <keselekpermen69@gmail.com>
2020-04-17 21:16:49 +0700

    pmpermit: change unapproved msg
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -17,12 +17,11 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 UNAPPROVED_MSG = (
-    "`Sorry, seems I haven't approved you to PM yet.\n`"
+    "`Sorry, I haven't approved you to PM yet.\n`"
     "`Please wait for me to look in.\n`"
-    "`Until then, please don't spam My PM...\n`"
+    "`Until then, please don't spam My PM or you will get blocked...\n`"
     "`Thank You\n\n`"
-    "`*This is an automated message.\n`"
-    "`❤️사나❤️`"  )
+    "`*This is an automated message.\n`")
 # =================================================================
 
 
