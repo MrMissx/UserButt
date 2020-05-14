f1b8dfb19f6142558767e4bf93d42a1570edf521
Mr.Miss <keselekpermen69@gmail.com>
2020-05-14 12:46:34 +0700

    treewide: Refactor licence from 1.c to 1.d
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>

--- userbot/modules/pics.py
+++ userbot/modules/pics.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
 from io import BytesIO
 from userbot.events import register
 from userbot import CMD_HELP
