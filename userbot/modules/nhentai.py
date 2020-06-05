a8b2b0fbb5644ff0fa4c5b96cb99071212295a0f
Mr.Miss <keselekpermen69@gmail.com>
2020-06-05 07:55:19 +0700

    a

--- userbot/modules/nhentai.py
+++ userbot/modules/nhentai.py
@@ -21,8 +21,10 @@ async def _(event):
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=424466890))
-              await bot.send_message(chat, link)
-              response = await response 
+              msg = await bot.send_message(chat, link)
+              response = await response
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError: 
               await event.reply("```Please unblock @nHentaiBot and try again```")
               return
@@ -31,8 +33,12 @@ async def _(event):
           else: 
              await event.delete()   
              await bot.send_message(event.chat_id, response.message)
+             """ - cleanup chat after completed - """
+             await event.client.delete_messages(conv.chat_id,
+                                                [msg.id, response.id])
+
 
 CMD_HELP.update({
 "nhentai": 
-"`.nhentai` <link / code> \
-\nUsage: view nhentai in telegra.ph XD\n"})
+"`.nhentai` <link / code>"
+"\nUsage: view nhentai in telegra.ph XD"})
