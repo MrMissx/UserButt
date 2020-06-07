57ecdf067e119b6c5214f72fc15efd5d0c41d16c
Mr.Miss <keselekpermen69@gmail.com>
2020-06-07 09:38:03 +0700

    a

--- userbot/modules/nhentai.py
+++ userbot/modules/nhentai.py
@@ -15,7 +15,9 @@ from userbot.events import register
 async def _(hentai):
     if hentai.fwd_from:
         return
-    link = hentai.pattern_match.group(1) 
+    link = hentai.pattern_match.group(1)
+	if not link:
+	    return await hentai.edit("`I can't search nothing`")
     chat = "@nHentaiBot"
     await hentai.edit("```Processing```")
     async with bot.conversation(chat) as conv:
@@ -30,15 +32,15 @@ async def _(hentai):
               return
           if response.text.startswith("**Sorry I couldn't get manga from**"):
              await hentai.edit("```I think this is not the right link```")
-          else: 
-             await hentai.delete()   
+          else:
+             await hentai.delete()		  
              await bot.send_message(hentai.chat_id, response.message)
+             await bot.send_read_acknowledge(hentai.chat_id)
              """ - cleanup chat after completed - """
              await hentai.client.delete_messages(conv.chat_id,
                                                 [msg.id, response.id])
 
-
 CMD_HELP.update({
 "nhentai": 
 "`.nhentai` <link / code>"
-"\nUsage: view nhentai in telegra.ph XD"})
+"\nUsage: view nhentai in telegra.ph"})
