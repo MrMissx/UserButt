c5d5615ca0f3065db3e6ce61850f7bef110916c8
Mr.Miss <keselekpermen69@gmail.com>
2020-06-07 10:06:54 +0700

    fix encoding

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -431,9 +431,9 @@ async def download_gdrive(gdrive, service, uri):
                     speed = round(downloaded / diff, 2)
                     eta = round((file_size - downloaded) / speed)
                     prog_str = "`Downloading` | [{0}{1}] `{2}%`".format(
-                        "".join(["°Ò" for i in range(
+                        "".join(["‚óè" for i in range(
                                 math.floor(percentage / 10))]),
-                        "".join(["°"for i in range(
+                        "".join(["‚óã"for i in range(
                                 10 - math.floor(percentage / 10))]),
                         round(percentage, 2))
                     current_message = (
@@ -477,9 +477,9 @@ async def download_gdrive(gdrive, service, uri):
                     speed = round(downloaded / diff, 2)
                     eta = round((file_size - downloaded) / speed)
                     prog_str = "`Downloading` | [{0}{1}] `{2}%`".format(
-                        "".join(["°Ò" for i in range(
+                        "".join(["‚óè" for i in range(
                                 math.floor(percentage / 10))]),
-                        "".join(["°" for i in range(
+                        "".join(["‚óã" for i in range(
                                 10 - math.floor(percentage / 10))]),
                         round(percentage, 2))
                     current_message = (
@@ -637,9 +637,9 @@ async def upload(gdrive, service, file_path, file_name, mimeType):
             speed = round(uploaded / diff, 2)
             eta = round((file_size - uploaded) / speed)
             prog_str = "`Uploading` | [{0}{1}] `{2}%`".format(
-                "".join(["°Ò" for i in range(
+                "".join(["‚óè" for i in range(
                         math.floor(percentage / 10))]),
-                "".join(["°" for i in range(
+                "".join(["‚óã" for i in range(
                         10 - math.floor(percentage / 10))]),
                 round(percentage, 2))
             current_message = (
@@ -1254,9 +1254,9 @@ async def check_progress_for_dl(gdrive, gid, previous):
                 percentage = int(file.progress)
                 downloaded = percentage * int(file.total_length) / 100
                 prog_str = "`Downloading` | [{0}{1}] `{2}`".format(
-                    "".join(["°Ò" for i in range(
+                    "".join(["‚óè" for i in range(
                             math.floor(percentage / 10))]),
-                    "".join(["°" for i in range(
+                    "".join(["‚óã" for i in range(
                             10 - math.floor(percentage / 10))]),
                     file.progress_string())
                 msg = (
