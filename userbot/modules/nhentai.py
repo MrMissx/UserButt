3e24d499318c25001c60ba33736feb6af2a454c6
Mr.Miss <keselekpermen69@gmail.com>
2020-06-07 08:11:11 +0700

    b

--- userbot/modules/nhentai.py
+++ userbot/modules/nhentai.py
@@ -12,12 +12,12 @@ from userbot import bot, CMD_HELP
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.nhentai(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
+async def _(hentai):
+    if hentai.fwd_from:
         return
-    link = event.pattern_match.group(1) 
+    link = hentai.pattern_match.group(1) 
     chat = "@nHentaiBot"
-    await event.edit("```Processing```")
+    await hentai.edit("```Processing```")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=424466890))
@@ -26,15 +26,15 @@ async def _(event):
               """ - don't spam notif - """
               await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @nHentaiBot and try again```")
+              await hentai.reply("```Please unblock @nHentaiBot and try again```")
               return
           if response.text.startswith("**Sorry I couldn't get manga from**"):
-             await event.edit("```I think this is not the right link```")
+             await hentai.edit("```I think this is not the right link```")
           else: 
-             await event.delete()   
-             await bot.send_message(event.chat_id, response.message)
+             await hentai.delete()   
+             await bot.send_message(hentai.chat_id, response.message)
              """ - cleanup chat after completed - """
-             await event.client.delete_messages(conv.chat_id,
+             await hentai.client.delete_messages(conv.chat_id,
                                                 [msg.id, response.id])
 
 
