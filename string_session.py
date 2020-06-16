407a630cc4bd3d03e186df764c0913d8ee6f308e
Mr.Miss <keselekpermen69@gmail.com>
2020-06-16 16:52:56 +0700

    a

--- string_session.py
+++ string_session.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677
+# (c) https://t.me/TelethonChat/37677 and UniBorg
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# (c) SpEcHiDe
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
@@ -18,8 +17,10 @@ API_KEY = int(input("Enter API_KEY here: "))
 API_HASH = input("Enter API_HASH here: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+    print("Check your Telegram Saved Messages to copy the STRING_SESSION value")
     session_string = client.session.save()
-    saved_messages_template = """@userbotindo
+    saved_messages_template = """Support: @userbotindo
+
 <code>STRING_SESSION</code>: <code>{}</code>
-⚠️ <i>It is forbidden to pass this value to third parties</i>""".format(session_string)
+⚠️ <i>Please be carefull to pass this value to third parties</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
