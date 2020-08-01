73fda1b8a1275c01160fdec0578b8c8f4d227912
aidilaryanto <aidilaryanto18@gmail.com>
2020-08-01 17:32:04 +0700

    sangmata: change method to search by id
    * squash change from:
        1. sangmata: change method to search by id [aidilaryanto/ProjectDils@316fbcd]
        2. sangmata: cleanup message after complete [aidilaryanto/ProjectDils@6078437]
        3. add exception if bot not respond [GengKapak/DCLXVI@7139dea]
    
    no more privacy XDD
    
    Co-authored-by: Angga <angga@linuxmail.org>
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -522,37 +522,44 @@ async def fryerrr(fry):
     return os.remove(downloaded_file_name)
 
 
-@register(outgoing=True, pattern="^.sg(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
 async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
         await steal.edit("`Reply to any user message.`")
         return
-    reply_message = await steal.get_reply_message()
-    if not reply_message.text:
-        await steal.edit("`reply to text message`")
-        return
+    message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
-    if reply_message.sender.bot:
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
         await steal.edit("`Reply to actual users message.`")
         return
-    await steal.edit("`Sit tight while I steal some data`")
-    async with bot.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=461843263))
-            await bot.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await steal.reply("`Please unblock @sangmatainfo_bot and try again`")
-            return
-        if response.text.startswith("Forward"):
-            await steal.edit("`can you kindly disable your forward privacy settings for good?`")
-        else:
-            await steal.edit(f"{response.message.message}")
+    await steal.edit("`Sit tight while I steal some data from NASA`")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await steal.reply("`Please unblock @sangmatainfo_bot and try again`")
+                return
+            if response.text.startswith("No records found"):
+                await steal.edit("`No records found for this user`")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            else:
+                respond = await conv.get_response()
+                await steal.edit(f"{response.message}")
+            await steal.client.delete_messages(
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
+            )
+    except TimeoutError:
+        return await steal.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
 
 
 @register(outgoing=True, pattern="^.waifu(?: |$)(.*)")
